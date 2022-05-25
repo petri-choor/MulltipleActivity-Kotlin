351d18b4776ca826f705ab3a503fa6a8700f46e3
peetr <peetrichoor@gmail.com>
2022-05-25 18:49:18 +0800

    Multiple-Activity-Login-Register

---
+++ app/src/main/java/com/example/authentication/HomeActivity.kt
@@ -0,0 +1,20 @@
+package com.example.authentication
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.authentication.databinding.ActivityHomeBinding
+
+class HomeActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHomeBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnLogout.setOnClickListener{
+            startActivity(Intent(this, LoginActivity::class.java))
+        }
+    }
+}
